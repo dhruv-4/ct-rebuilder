cb049f9c86bc6872db082e394e6baf7f096895dc
dhruv-4 <dhruv.datt@comtravo.com>
2021-08-12 23:10:41 +0200

    🚧  use custom view

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { FileExplorer } from "./fileExplorer";
+import { TestView } from "./testView";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -24,7 +24,9 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 
-  new FileExplorer(context);
+  const folders = vscode.workspace.workspaceFolders || [];
+
+  new TestView(context, folders[0]?.uri?.fsPath);
 }
 
 // this method is called when your extension is deactivated
